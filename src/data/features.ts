48a8b835414b38861e904de3e9356bde3c090053
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2026-04-05 14:45:36 -0500

    Add features data file

---
+++ src/data/features.ts
@@ -0,0 +1,23 @@
+const features = [
+  {
+    title: "Simple Bookmarking",
+    image: "/src/assets/illustration-features-tab-1.png",
+    header: "Bookmark in one click",
+    content:
+      "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control.",
+  },
+  {
+    title: "Speedy Searching",
+    image: "/src/assets/illustration-features-tab-2.png",
+    header: "Intelligent search",
+    content: "Our powerful search feature will help you find saved sites in no time at all.",
+  },
+  {
+    title: "Easy Sharing",
+    image: "/src/assets/illustration-features-tab-3.png",
+    header: "Share your bookmarks",
+    content: "Easily share your bookmarks and collections with others.",
+  },
+];
+
+export default features;
