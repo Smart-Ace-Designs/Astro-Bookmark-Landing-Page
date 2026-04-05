5d30ec5dd130b3a00071f0da5a2bd468f8f4f344
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2026-04-05 14:58:19 -0500

    Add initial scripting for tabs

--- src/data/features.ts
+++ src/data/features.ts
@@ -3,20 +3,22 @@ const features = [
     title: "Simple Bookmarking",
     image: "/src/assets/illustration-features-tab-1.png",
     header: "Bookmark in one click",
-    content:
-      "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control.",
+    description:
+      "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.",
   },
   {
     title: "Speedy Searching",
     image: "/src/assets/illustration-features-tab-2.png",
     header: "Intelligent search",
-    content: "Our powerful search feature will help you find saved sites in no time at all.",
+    description:
+      "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.",
   },
   {
     title: "Easy Sharing",
     image: "/src/assets/illustration-features-tab-3.png",
     header: "Share your bookmarks",
-    content: "Easily share your bookmarks and collections with others.",
+    description:
+      "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.",
   },
 ];
 
