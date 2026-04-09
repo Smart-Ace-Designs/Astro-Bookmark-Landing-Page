6ae95018c46b07a8f8974ffa19b105d9e0acccea
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2026-04-09 08:46:53 -0500

    Add accordian functionality for faq section

--- src/data/faqs.ts
+++ src/data/faqs.ts
@@ -1,22 +1,22 @@
 const faqs = [
   {
     question: "What is Bookmark?",
-    anwser:
+    answer:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis.",
   },
   {
     question: "How can I request a new browser?",
-    anwser:
+    answer:
       "Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet. Vivamus luctus eros aliquet convallis ultricies. auris augue massa, ultricies non ligula. Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet.",
   },
   {
     question: "Is there a mobile app",
-    anwser:
+    answer:
       "Sed consectetur quam id neque fermentum accumsan. Praesent luctus vestibulum dolor, ut condimentum urna vulputate eget. Cras in ligula quis est pharetra mattis sit amet pharetra purus. Sed sollicitudin ex et ultricies bibendum.",
   },
   {
     question: "What about other Chromium browsers?",
-    anwser:
+    answer:
       "Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat mi, at euismod dui. Aliquam vitae neque eget nisl gravida pellentesque non ut velit.",
   },
 ];
