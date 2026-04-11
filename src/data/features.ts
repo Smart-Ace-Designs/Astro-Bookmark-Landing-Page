a91927893e1fcd629c22812323dfd54fef1b12aa
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2026-04-11 13:03:11 -0500

    Testing broken Netlify images

--- src/data/features.ts
+++ src/data/features.ts
@@ -1,21 +1,21 @@
 const features = [
   {
     title: "Simple Bookmarking",
-    image: "/src/assets/illustration-features-tab-1.png",
+    image: "/illustration-features-tab-1.png",
     header: "Bookmark in one click",
     description:
       "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.",
   },
   {
     title: "Speedy Searching",
-    image: "/src/assets/illustration-features-tab-2.png",
+    image: "/illustration-features-tab-2.png",
     header: "Intelligent search",
     description:
       "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.",
   },
   {
     title: "Easy Sharing",
-    image: "/src/assets/illustration-features-tab-3.png",
+    image: "/illustration-features-tab-3.png",
     header: "Share your bookmarks",
     description:
       "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.",
